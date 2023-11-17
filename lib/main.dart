3ddcdd98f52da9635c13b8072d1db7eda7f2ec1a
Alejazzz <alejandrofutboleromolon@gmail.com>
2023-11-17 18:15:14 +0100

    17-11

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:ejercimente/screens/main_page.dart';
 import 'package:ejercimente/setApp/setAppUrl.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 //importaciones firebase
 import 'package:firebase_core/firebase_core.dart';
-import 'package:intl/date_symbol_data_local.dart';
 import 'firebase_options.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
--- lib/screens/calendar.dart
+++ lib/screens/calendar.dart
@@ -1,7 +1,8 @@
 import 'package:ejercimente/screens/screen_template.dart';
+import 'package:ejercimente/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
-
+import 'package:ejercimente/utils/event.dart';
 
 class CalendarPage extends StatefulWidget {
   @override
@@ -9,6 +10,38 @@ class CalendarPage extends StatefulWidget {
 }
 
 class _CalendarPageState extends State<CalendarPage> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+  Map<DateTime, List<Event>> events = {};
+  TextEditingController _eventController = TextEditingController();
+  late final ValueNotifier<List<Event>> _selectedEvents;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedDay = _focusedDay;
+    _selectedEvents = ValueNotifier(_getEventsForDay(_selectedDay!));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    if(!isSameDay(_selectedDay, selectedDay)) {
+      setState(() {
+        _selectedDay = selectedDay;
+        _focusedDay = focusedDay;
+        _selectedEvents.value = _getEventsForDay(selectedDay);
+      });
+    }
+  }
+
+  List<Event> _getEventsForDay(DateTime day) {
+    return events[day] ?? [];
+  }
 
 
   @override
@@ -22,6 +55,91 @@ class _CalendarPageState extends State<CalendarPage> {
               firstDay: DateTime.utc(2010, 10, 16),
               lastDay: DateTime.utc(2030, 3, 14),
               focusedDay: DateTime.now(),
+              selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+              calendarFormat: _calendarFormat,
+              startingDayOfWeek: StartingDayOfWeek.monday,
+              onDaySelected: _onDaySelected,
+              eventLoader: _getEventsForDay,
+              calendarStyle: const CalendarStyle(
+                outsideDaysVisible: false,
+              ),
+              onFormatChanged: (format) {
+                if (_calendarFormat != format) {
+                  setState(() {
+                    _calendarFormat = format;
+                  });
+                }
+              },
+              onPageChanged: (focusedDay) {
+                _focusedDay = focusedDay;
+              },
+            ),
+            ValueListenableBuilder<List<Event>>(
+              valueListenable: _selectedEvents,
+              builder: (context, value, _){
+                return ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: value.length,
+                  itemBuilder: (context, index) {
+                    Event currentEvent = value[index];
+                    return Container(
+                      margin: EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+                      decoration: BoxDecoration(
+                        border: Border.all(),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: ListTile(
+                        onTap: () => print(""),
+                        title: Text(currentEvent.title),
+                      ),
+                    );
+                  }
+                );
+              }
+            ),
+            InkWell(
+              onTap: (){
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      scrollable: true,
+                      title: Text("Nombre del evento"),
+                      content: Padding(
+                        padding: EdgeInsets.all(8),
+                        child: TextField(
+                          controller: _eventController,
+                        ),
+                      ),
+                      actions: [
+                        ElevatedButton(
+                            onPressed: () {
+                              final selectedDay = _selectedDay!;
+                              final newEvent = Event(_eventController.text);
+                              events.update(
+                                selectedDay,
+                                    (existingEvents) {
+                                  existingEvents.add(newEvent);
+                                  return existingEvents;
+                                },
+                                ifAbsent: () => [newEvent],
+                              );
+                              Navigator.of(context).pop();
+                              setState(() {
+                                _selectedEvents.value = _getEventsForDay(selectedDay);
+                              });
+                            },
+                            child: Text("Agregar"))
+                      ],
+                    );
+                  },
+                );
+              },
+              child: Container(
+                height: 100,
+                width: 100,
+                color: widgets_background_brown,
+              ),
             )
           ],
         )
